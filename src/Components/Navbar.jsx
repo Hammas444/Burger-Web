0b982249236788aea7f4338185822a860d3deede
M. Hammas Sheikh <113780545+Hammas444@users.noreply.github.com>
2025-05-30 23:32:38 +0500

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -22,13 +22,13 @@ function Navbar() {
 
   return (
     <div className="container-fluid">
-      <nav
+      {/* <nav
         className={`navbar fixed-top navbar-expand-lg ${
           scroll ? 'scrolled-navbar' : 'transparent-navbar'
         }`}
       >
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/home">
+          <a className="navbar-brand" href="/home">
             <span
               style={{
                 fontFamily: '"Pacifico", cursive',
@@ -39,7 +39,7 @@ function Navbar() {
             >
               Burger O'Clock
             </span>
-          </Link>
+          </a>
           <span>
             <button
               className="navbar-toggler"
@@ -128,7 +128,124 @@ function Navbar() {
             </div>
           </span>
         </div>
-      </nav>
+      </nav> */}
+
+
+
+<nav
+  className={`navbar fixed-top navbar-expand-lg ${
+    scroll ? 'scrolled-navbar' : 'transparent-navbar'
+  }`}
+>
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">
+      <span
+        style={{
+          fontFamily: '"Pacifico", cursive',
+          color: scroll ? '#f87721' : '#ffffff',
+          marginLeft: 5,
+          fontSize: 20,
+        }}
+      >
+        Burger O'Clock
+      </span>
+    </a>
+
+    {/* TOGGLER BUTTON (Only visible on mobile) */}
+    <button
+      className="navbar-toggler d-lg-none"
+      type="button"
+      data-bs-toggle="offcanvas"
+      data-bs-target="#offcanvasExample"
+      aria-controls="offcanvasExample"
+      aria-label="Toggle navigation"
+    >
+      <i className="fa-solid fa-bars" style={{ color: '#ffd034' }} />
+    </button>
+
+    {/* DESKTOP NAV LINKS */}
+    <div className="collapse navbar-collapse d-none d-lg-block" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0 desktop-nav">
+        <li className="nav-item">
+          <a className="nav-link desktop-link" href="/home" style={{ color: scroll ? '#000' : '#fff' }}>
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link desktop-link" href="/About" style={{ color: scroll ? '#000' : '#fff' }}>
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link desktop-link" href="/meals" style={{ color: scroll ? '#000' : '#fff' }}>
+            Meals
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link desktop-link" href="/TableReservation" style={{ color: scroll ? '#000' : '#fff' }}>
+            Reservation
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  {/* OFFCANVAS NAV LINKS */}
+  <div
+    className="offcanvas offcanvas-start d-lg-none"
+    tabIndex={-1}
+    id="offcanvasExample"
+    aria-labelledby="offcanvasExampleLabel"
+    style={{ backgroundColor: '#ffffff' }}
+  >
+    <div className="offcanvas-header">
+      <h5 className="offcanvas-title" id="offcanvasExampleLabel">
+        <span
+          style={{
+            fontFamily: '"Pacifico", cursive',
+            color: '#ffd034',
+            marginLeft: 5,
+            fontSize: 20,
+          }}
+        >
+          Burger O'Clock
+        </span>
+      </h5>
+      <button
+        type="button"
+        className="btn-close text-reset"
+        data-bs-dismiss="offcanvas"
+        aria-label="Close"
+      />
+    </div>
+    <div className="offcanvas-body">
+      <ul className="navbar-nav mx-auto mb-2 mb-lg-0 offcanvas-nav">
+        <li className="nav-item">
+          <a className="nav-link offcanvas-link" href="/home" style={{ color: scroll ? '#000' : '#000' }}>
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link offcanvas-link" href="/About" style={{ color: scroll ? '#000' : '#000' }}>
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link offcanvas-link" href="/meals" style={{ color: scroll ? '#000' : '#000' }}>
+            Meals
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link offcanvas-link" href="/TableReservation" style={{ color: scroll ? '#000' : '#000' }}>
+            Reservation
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
     </div>
   );
 }
