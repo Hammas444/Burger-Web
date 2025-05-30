bfc054702a46c343f3b728fd651a3bff575d196c
M. Hammas Sheikh <113780545+Hammas444@users.noreply.github.com>
2025-05-30 23:52:31 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
 <Route path='/' element={<Login/>}/>
 <Route path='/home' element={<Home/>}/>
-<Route path='/About' element={<About/>}/>
+<Route path='/about' element={<About/>}/>
 <Route path='/meals' element={<Meals/>}/>
 <Route path='/TableReservation' element={<TableReserve/>}/>
 
