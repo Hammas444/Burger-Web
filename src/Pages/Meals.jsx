f53b8a61f6e0e1e533545fc98020676fcce57247
M. Hammas Sheikh <113780545+Hammas444@users.noreply.github.com>
2025-05-31 00:09:27 +0500

    Update Meals.jsx

--- src/Pages/Meals.jsx
+++ src/Pages/Meals.jsx
@@ -31,7 +31,7 @@ const Meals = () => {
           <div className="col-lg-4 col-sm-12 col-md-6">
             <div className="card mt-2" style={{width: '18rem', border: 'none'}}>
               <div className="card-body" style={{backgroundColor: '#ffffff'}}>
-                <a href="#"><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
+                <a ><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
                     <div className="carousel-inner">
                       <div className="carousel-item active">
                         <img src="pasta-dishes-lead-1-1588367700.jpg" className="d-block w-100" alt="..." />
@@ -41,7 +41,7 @@ const Meals = () => {
                     </div>
                   </div></a>
                 <br />
-                <a href="#" className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g1()">
+                <a  className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g1()">
                   Get Now</a>
               </div>
             </div>
@@ -49,7 +49,7 @@ const Meals = () => {
           <div className="col-lg-4 col-sm-12 col-md-6">
             <div className="card mt-2" style={{width: '18rem', border: 'none'}}>
               <div className="card-body">
-                <a href="#"><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+                <a ><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
                     <div className="carousel-inner">
                       <div className="carousel-item active">
                         <img src="sand.jpg" className="d-block w-100" alt="..." />
@@ -59,14 +59,14 @@ const Meals = () => {
                     </div>
                   </div></a>
                 <br />
-                <a href="#" className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g2()">Get Now</a>
+                <a  className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g2()">Get Now</a>
               </div>
             </div>
           </div>
           <div className="col-lg-4 col-sm-12 col-md-6">
             <div className="card mt-2" style={{width: '18rem', border: 'none'}}>
               <div className="card-body">
-                <a href="#"><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+                <a ><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
                     <div className="carousel-inner">
                       <div className="carousel-item active">
                         <img src="ice (2).jpg" className="d-block w-100" alt="..." />
@@ -76,7 +76,7 @@ const Meals = () => {
                     </div>
                   </div></a>
                 <br />
-                <a href="#" className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g3()">Get Now</a>
+                <a  className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g3()">Get Now</a>
               </div>
             </div>
           </div>
@@ -87,7 +87,7 @@ const Meals = () => {
           <div className="col-lg-4 col-sm-12 col-md-6">
             <div className="card mt-2" style={{width: '18rem', border: 'none'}}>
               <div className="card-body" style={{backgroundColor: '#ffffff'}}>
-                <a href="#"><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
+                <a ><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
                     <div className="carousel-inner">
                       <div className="carousel-item active">
                         <img src="c1.jpg" className="d-block w-100" alt="..." />
@@ -97,7 +97,7 @@ const Meals = () => {
                     </div>
                   </div></a>
                 <br />
-                <a href="#" className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g4()">
+                <a  className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g4()">
                   Get Now</a>
               </div>
             </div>
@@ -105,7 +105,7 @@ const Meals = () => {
           <div className="col-lg-4 col-sm-12 col-md-6">
             <div className="card mt-2" style={{width: '18rem', border: 'none'}}>
               <div className="card-body">
-                <a href="#"><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+                <a ><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
                     <div className="carousel-inner">
                       <div className="carousel-item active">
                         <img src="ice-cream-sandwich-cake-featured-500x500.webp" className="d-block w-100" alt="..." />
@@ -115,14 +115,14 @@ const Meals = () => {
                     </div>
                   </div></a>
                 <br />
-                <a href="#" className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g5()">Get Now</a>
+                <a  className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g5()">Get Now</a>
               </div>
             </div>
           </div>
           <div className="col-lg-4 col-sm-12 col-md-6">
             <div className="card mt-2" style={{width: '18rem', border: 'none'}}>
               <div className="card-body">
-                <a href="#"><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+                <a ><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
                     <div className="carousel-inner">
                       <div className="carousel-item active">
                         <img src="shutterstock_1773695441-min-600x600.jpg" className="d-block w-100" alt="..." />
@@ -132,7 +132,7 @@ const Meals = () => {
                     </div>
                   </div></a>
                 <br />
-                <a href="#" className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g6()">Get Now</a>
+                <a  className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g6()">Get Now</a>
               </div>
             </div>
           </div>
@@ -143,7 +143,7 @@ const Meals = () => {
           <div className="col-lg-4 col-sm-12 col-md-6">
             <div className="card mt-2" style={{width: '18rem', border: 'none'}}>
               <div className="card-body" style={{backgroundColor: '#ffffff'}}>
-                <a href="#"><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
+                <a ><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
                     <div className="carousel-inner">
                       <div className="carousel-item active">
                         <img src="bro.jpg" className="d-block w-100" alt="..." />
@@ -153,7 +153,7 @@ const Meals = () => {
                     </div>
                   </div></a>
                 <br />
-                <a href="#" className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g7()">
+                <a  className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g7()">
                   Get Now</a>
               </div>
             </div>
@@ -161,7 +161,7 @@ const Meals = () => {
           <div className="col-lg-4 col-sm-12 col-md-6">
             <div className="card mt-2" style={{width: '18rem', border: 'none'}}>
               <div className="card-body">
-                <a href="#"><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+                <a ><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
                     <div className="carousel-inner">
                       <div className="carousel-item active">
                         <img src="piza.jpg" className="d-block w-100" alt="..." />
@@ -171,14 +171,14 @@ const Meals = () => {
                     </div>
                   </div></a>
                 <br />
-                <a href="#" className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g8()">Get Now</a>
+                <a  className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g8()">Get Now</a>
               </div>
             </div>
           </div>
           <div className="col-lg-4 col-sm-12 col-md-6">
             <div className="card mt-2" style={{width: '18rem', border: 'none'}}>
               <div className="card-body">
-                <a href="#"><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+                <a ><div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
                     <div className="carousel-inner">
                       <div className="carousel-item active">
                         <img src="fish.jpg" className="d-block w-100" alt="..." />
@@ -188,7 +188,7 @@ const Meals = () => {
                     </div>
                   </div></a>
                 <br />
-                <a href="#" className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g9()">Get Now</a>
+                <a  className="btn btn-" style={{background: 'linear-gradient(to right,#ff8411,#ff3a17)', color: 'white', border: 'none', borderRadius: '0%', width: 250, boxShadow: '2px 2px 10px rgba(105, 105, 105, 0.541)'}} onclick="g9()">Get Now</a>
               </div>
             </div>
           </div>
