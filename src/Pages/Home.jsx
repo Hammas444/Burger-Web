a9fecc7dc3b5c6a63fc2982ae0c15d9b251a9eb5
M. Hammas Sheikh <113780545+Hammas444@users.noreply.github.com>
2025-05-30 23:49:01 +0500

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Navbar from '../Components/Navbar'
+
 import Footer from '../Components/Footer'
 
 const Home = () => {
   return (
     <>
-<Navbar/>
+
 
 <div>
   <section>
